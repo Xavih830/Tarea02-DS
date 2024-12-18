c6b12d5eb598b4f5a4aefdef603186818f2c1770
Andres Layedra <alayedra@espol.edu.ec>
2024-12-18 16:30:12 -0500

    ..

--- SportsPredictor/Notificadores.java
+++ SportsPredictor/Notificadores.java
@@ -1,3 +1,5 @@
 public interface Notificadores {
     public String enviarNotificacion(Usuario usuario, String menssaje);
+
+
 }
--- SportsPredictor/ObserverNotificaciones.java
+++ SportsPredictor/ObserverNotificaciones.java
@@ -1,6 +1,9 @@
-public class ObserverNotificaciones {
-    private Notificadores[] observadores;
 
+
+
+public class ObserverNotificaciones {
+
+    
     public void notificar(String mensaje){};
     public void subscribir(Usuario usuario){};
 }

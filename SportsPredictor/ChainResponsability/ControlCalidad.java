0b29ca7bedf13b9b5ea242a4962f2b8d7426167f
Andres Layedra <alayedra@espol.edu.ec>
2025-01-07 11:44:54 -0500

    .

--- SportsPredictor/ChainResponsability/ControlCalidad.java
+++ SportsPredictor/ChainResponsability/ControlCalidad.java
@@ -1,8 +1,19 @@
 package chainresponsability;
 
-public class ControlCalidad extends BaseManejador{
+public class ControlCalidad implements ManejadorReporte{
+    private ManejadorReporte siguiente;
 
-
+    @Override
+    public void setNext(ManejadorReporte manejador) {
+        if(siguiente == null){
+            siguiente = manejador;
+        }
+    }
+    
+    @Override
+    public void manejarReporte(Reporte reporte) {
+        System.out.println("Reporte Manejado por Control Calidad");
+    }
 
 
 }
--- SportsPredictor/ChainResponsability/Soporte.java
+++ SportsPredictor/ChainResponsability/Soporte.java
@@ -1,7 +1,14 @@
 package chainresponsability;
 
 
-public class Soporte extends BaseManejador {
+public class Soporte implements ManejadorReporte{
+    private ManejadorReporte siguiente;
+
+    @Override
+    public void setNext(ManejadorReporte manejador) {
+        siguiente = manejador;
+    }
+
 
     @Override
     public void manejarReporte(Reporte reporte) {

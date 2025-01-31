603c1e1ec79d8e4f89e6c89ba38ec954d7e3ed13
carrbrus <139665813+carrbrus@users.noreply.github.com>
2025-01-31 00:55:40 -0500

    Update and rename Deportes.java to DeporteFactory.java

--- SportsPredictor/FactoryMethod/DeporteFactory.java
+++ SportsPredictor/FactoryMethod/DeporteFactory.java
@@ -4,7 +4,7 @@ import P_SportsPredictor.*;
 import java.time.LocalDateTime;
 import java.util.List;
 
-public interface Deportes {
+public interface DeporteFactory {
     void crearEvento(String nombre, LocalDateTime fecha, List<Pronostico> pronosticos, 
                      List<Estadistica> participantes, List<Equipos> equipos);
 }
